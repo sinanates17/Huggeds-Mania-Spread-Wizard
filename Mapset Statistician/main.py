063e8f00745822558a1067b204ef95d5cfd62857
sinanates17 <sinanates17@gmail.com>
2024-02-21 23:35:29 -0500

    wtuff

--- Mapset Statistician/main.py
+++ Mapset Statistician/main.py
@@ -8,5 +8,6 @@ from Interface.main_window import MainWindow
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.move(100,100)
     window.show()
     sys.exit(app.exec_())
